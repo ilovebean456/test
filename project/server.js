1efd74cf1bf1aa85584850ba62fe7510846c5608
ilovebean456 <144631932+ilovebean456@users.noreply.github.com>
2024-02-04 13:11:28 -0500

    Add files via upload

--- project/server.js
+++ project/server.js
@@ -1,4 +1,5 @@
 var password = "31415";
+var question = "yes";
 
 function passcheck() {
     if(document.getElementById('pass1').value != password) {
@@ -7,6 +8,15 @@ function passcheck() {
     }
     
      if(document.getElementById('pass1').value == password) {
+          alert('Correct!');
+      }
+
+      if(document.getElementById('pass2').value != question) {
+        alert('Wrong password, try again.');
+        return false;
+    }
+    
+     if(document.getElementById('pass2').value == question) {
           alert('Correct!');
       }
     
