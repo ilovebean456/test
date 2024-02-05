897b4ba479359da379d0eff5f6d9096745a6dd3a
ilovebean456 <144631932+ilovebean456@users.noreply.github.com>
2024-02-05 12:21:22 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var password = "31415";
+var question = "yes";
+
+function passcheck() {
+    if(document.getElementById('pass1').value != password) {
+        alert('Wrong password, try again.');
+        return false;
+    }
+    
+     if(document.getElementById('pass1').value == password) {
+          alert('Correct!');
+      }
+}
+
+function passcheck2(){
+    if(document.getElementById('pass2').value != question) {
+        alert('WRONG ANSWER');
+        return false;
+    }
+    else if(document.getElementById('pass2').value != question)
+    
+     if(document.getElementById('pass2').value == question) {
+          alert('YIPPEE');
+      }
+    
+}
